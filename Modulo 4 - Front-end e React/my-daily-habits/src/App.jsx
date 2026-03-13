61a5811e99fd6321ed9ba906dd03187d725d110b
Fesisp <mrfelipefss@gmail.com>
2026-03-13 19:05:01 -0400

    modified:   Modulo 4 - Front-end e React/my-daily-habits/src/App.css 	modified:   Modulo 4 - Front-end e React/my-daily-habits/src/App.jsx 	deleted:    Modulo 4 - Front-end e React/my-daily-habits/src/New.css 	modified:   Modulo 4 - Front-end e React/my-daily-habits/src/index.css

--- Modulo 4 - Front-end e React/my-daily-habits/src/App.jsx
+++ Modulo 4 - Front-end e React/my-daily-habits/src/App.jsx
@@ -1,23 +1,24 @@
+// src/App.jsx
 import './App.css'
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import BemVindo from "./components/BemVindo"
 import SecaoHabitos from "./components/SecaoHabitos"
 import HabitList from "./components/HabitList"
-import HabitCard from "./components/HabitCard"
 
 function App() {
   const habits = [
     { id: 1, titulo: 'Exercicio', meta: 5, ativo: true, diasFeitos: 5 },
     { id: 2, titulo: 'Leitura', meta: 5, ativo: true, diasFeitos: 5 },
     { id: 3, titulo: 'Meditação', meta: 5, ativo: true, diasFeitos: 5 },
-    { id: 4, titulo: 'Hidratação', meta: 5, ativo: true, diasFeitos: 5 }
+    { id: 4, titulo: 'Hidratação', meta: 5, ativo: true, diasFeitos: 5 },
   ]
   return (
       <div>
         <Header />
         <BemVindo nomeUsuario="turma iteam" totalHabitos={habits.length} />
         <SecaoHabitos titulo="Meus Hábitos" />
+         <HabitList habits={habits} />
         <SecaoHabitos habits={habits} />
         <Footer />
       </div>
